5466238a9afb3f7d0622c3f00c0e9901caddfed9
Theodor Nistor <115567983+Ted-Nistor@users.noreply.github.com>
2023-01-21 13:31:49 +0000

    Sat, Jan 21, 2023, 1:31 PM +00:00

--- hello.c
+++ hello.c
@@ -7,7 +7,7 @@ int main(void){
     string first = get_string("What's your first name? ");
     string last = get_string("What's your last name? ");
     printf("hello, %s %s\n",first,last);
-    string name = getc("What's your nickname? ");
+    char name = fgetc("What's your nickname? ");
     printf("hello, %s!\n",name);
 }
 
