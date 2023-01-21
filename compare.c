ea93f9c5baac97cfc444226f68297b6e99e0d9cd
Theodor Nistor <115567983+Ted-Nistor@users.noreply.github.com>
2023-01-21 14:07:56 +0000

    Sat, Jan 21, 2023, 2:07 PM +00:00

--- compare.c
+++ compare.c
@@ -4,13 +4,16 @@
 int main(void){
     int x= get_int("What's x? ");
     int y= get_int("What's y? ");
-    if(x<y){
+    if(x<y)
+    {
         printf("x is less than y\n");
     }
-    else if(x>y){
+    else if(x>y)
+    {
         printf("x is greater than y\n");
     }
-    else{
+    else
+    {
         printf("x is equal to y\n");
     }
 }
