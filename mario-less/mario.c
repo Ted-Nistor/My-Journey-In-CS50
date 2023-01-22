ab7553eaf696f35db11c3fad47a27496041b687d
Theodor Nistor <115567983+Ted-Nistor@users.noreply.github.com>
2023-01-22 22:45:50 +0000

    Sun, Jan 22, 2023, 10:45 PM +00:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -4,4 +4,14 @@
 int main(void)
 {
     int height = get_int("Height: ");
+    char space = ' ';
+    char bricks='#';
+    for (int i = 0 ; i < height ; i ++)
+    {
+        for (int j = 0 ; j <height ; j++)
+        {
+            printf("%c",bricks);
+        }
+        printf("\n");
+    }
 }
