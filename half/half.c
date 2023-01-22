fa9166370d6d7a7ace0ba2c6a55ee6535af7a015
Theodor Nistor <115567983+Ted-Nistor@users.noreply.github.com>
2023-01-22 21:27:01 +0000

    Sun, Jan 22, 2023, 9:27 PM +00:00

--- half/half.c
+++ half/half.c
@@ -18,5 +18,5 @@ int main(void)
 // TODO: Complete the function
 float half(float bill, float tax, int tip)
 {
-    float total = bill * (1+ tax/100 )
+    float total = bill * (1+ tax/100 ) * (1+ tip/100)
 }
