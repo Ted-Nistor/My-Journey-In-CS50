177d8c4b6e64114931fa41c8abebf6c6a7260a31
Theodor Nistor <115567983+Ted-Nistor@users.noreply.github.com>
2023-01-22 14:10:11 +0000

    Sun, Jan 22, 2023, 2:10 PM +00:00

--- mario.c
+++ mario.c
@@ -4,20 +4,33 @@
 int main(void)
 {
     //Get size of grid
-  int n;
-  do
-  {
-    n= get_int("Size: ");
-  }
-  while(n<1);
-// Print grid of bricks
- for (int i=0;i<n;i++)
- {
-    for(int j=0;j<n;j++)
-    {
-        printf("#");
-    }
-    printf("\n");
- }
+    int n=get_size();
 
+// Print grid of bricks
+    void 
+
+}
+
+
+int get_size(void)
+{
+    int n;
+    do
+    {
+        n= get_int("Size: ");
+    }
+    while(n<1);
+    return n;
+}
+
+void print_grid(int size)
+{
+    for (int i=0;i<size;i++)
+    {
+        for(int j=0;j<size;j++)
+        {
+        printf("#");
+        }
+    printf("\n");
+    }
 }
