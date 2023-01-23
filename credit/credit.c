5d365ad583b388e16cc1cd0a0d7f543a94dc1585
Theodor Nistor <115567983+Ted-Nistor@users.noreply.github.com>
2023-01-23 12:18:36 +0000

    Mon, Jan 23, 2023, 12:18 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -41,12 +41,20 @@ int check_digits(long x)
 {
     int first_set=0;
     int digits=0;
-    do
+    while(x>0)
     {
-        digits= x%10;
-        x/=10;
+        digits = x%10;
+        printf("%d",digits);
+        x = x/10;
+        if(digits*2 > 9)
+        {
+            int counter;
+            while (digits > 0)
+            {
+
+            }
+        }
     }
-    while(x > 0);
-    return digits;
+    return first_set
 }
 
