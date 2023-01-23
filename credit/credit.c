b998fa9a74427fb99bd940e77149e729e065f293
Theodor Nistor <115567983+Ted-Nistor@users.noreply.github.com>
2023-01-23 10:32:07 +0000

    Mon, Jan 23, 2023, 10:32 AM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,7 +1,21 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+long prompt_card(void);
+int main(void)
 {
-    
+    long card = prompt_card();
+    printf("Card Number: %li\n", card);
+}
+
+
+long prompt_card(void)
+{
+    long card;
+    do
+    {
+        card = get_long("Enter your credit card number: ");
+    }
+    while (card < 1);
+    return card;
 }
