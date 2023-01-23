979d2e362d2af9d04a1e98c3e222bee3c6443061
Theodor Nistor <115567983+Ted-Nistor@users.noreply.github.com>
2023-01-23 08:58:15 +0000

    Mon, Jan 23, 2023, 8:58 AM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -1,13 +1,29 @@
 #include <cs50.h>
 #include <stdio.h>
 
+int get_size (void);
 int main(void)
 {
+//Prompt the height of the blocks
+    int height = get_size();
+    printf("%i\n",height);
+//Print the number of rows equal to height ;
+    for (int rows = 0 ; rows < height ; rows++)
+    {
+//Print spaces before #
+        for (int space = (height-rows-1); space > 0 ; )
+    }
 
 }
 
 
-int get_size (void)
+int get_size ()
 {
-    int height;
+    int n;
+    do
+    {
+        n = get_int("Height: ");
+    }
+    while (n < 1 || n > 8);
+    return n;
 }
