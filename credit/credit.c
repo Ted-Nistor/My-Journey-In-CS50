bef29b05a286172cd0e58bd6df2305ccbf33a107
Theodor Nistor <115567983+Ted-Nistor@users.noreply.github.com>
2023-01-23 12:29:12 +0000

    Mon, Jan 23, 2023, 12:29 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -3,15 +3,13 @@
 
 long prompt_card(void);
 int length(long x);
-int check_digits(long x);
+
 int main(void)
 {
     long card = prompt_card();
     printf("Card Number: %li\n", card);
     int card_length = length(card);
     printf("Card Length: %i\n", card_length);
-    int first_set = check_digits(card);
-    printf("First Set: %i\n",first_set);
 }
 
 
@@ -35,10 +33,5 @@ int length(long x)
         counter++;
     }
     return counter;
-}
-
-int check_digits(long x)
-{
-    
 }
 
