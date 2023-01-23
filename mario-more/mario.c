cc2fbc15bdcd618973945f676500ea70713d0051
Theodor Nistor <115567983+Ted-Nistor@users.noreply.github.com>
2023-01-23 09:05:49 +0000

    Mon, Jan 23, 2023, 9:05 AM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -11,7 +11,23 @@ int main(void)
     for (int rows = 0 ; rows < height ; rows++)
     {
 //Print spaces before #
-        for (int space = (height-rows-1); space > 0 ; )
+        for (int space = (height-rows-1); space > 0 ; space-- )
+        {
+            printf(" ");
+        }
+//Print # after space
+        for (int hash = 0 ; hash <= rows ; hash++)
+        {
+            printf("#");
+        }
+//Print a space between the two pyramids
+        printf("  ");
+//Print right side of hashes
+        for (int right_hash = 0 ; right_hash <= rows ; right_hash++)
+        {
+            printf("#");
+        }
+    printf("\n");
     }
 
 }
