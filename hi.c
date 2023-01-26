be7e67612b1315f01fb444cafbf4057527f50d46
Theodor Nistor <115567983+Ted-Nistor@users.noreply.github.com>
2023-01-26 13:53:51 +0000

    Thu, Jan 26, 2023, 3:53 PM +02:00

--- hi.c
+++ hi.c
@@ -9,7 +9,7 @@ int main(void)
     words[1] = "BYE!";
 
     printf ("%c%c%c\n", words[0][0], words[0][1], words[0][2]);
-    printf ("%c%c%c\n", words[1][0], words[1][1], words[1][2], words[1][3]);
+    printf ("%c%c%c%c\n", words[1][0], words[1][1], words[1][2], words[1][3]);
 
 
 }
