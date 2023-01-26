9e15b7a908255de16f83d7c4963c46a770a0f657
Theodor Nistor <115567983+Ted-Nistor@users.noreply.github.com>
2023-01-26 13:45:29 +0000

    Thu, Jan 26, 2023, 3:45 PM +02:00

--- hi.c
+++ hi.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
+#include <cs50.h>
 
 int main(void)
 {
-    char c1 = 'H';
-    char c2 = 'I';
-    char c3 = '!';
-
-    printf ("%c%c%c\n", c1, c2, c3);
+    string s = "HI!";
+    printf ("%c%c%c\n", s[0], s[1], s[2]);
 }
