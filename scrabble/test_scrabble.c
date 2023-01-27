4f183412a186d2174b7ccf9ac0ab1511a1b5b1c1
Theodor Nistor <115567983+Ted-Nistor@users.noreply.github.com>
2023-01-27 10:33:41 +0000

    Fri, Jan 27, 2023, 12:33 PM +02:00

--- scrabble/test_scrabble.c
+++ scrabble/test_scrabble.c
@@ -3,16 +3,19 @@
 #include <string.h>
 
 
+void get_ascii(string word);
 int main(void)
 {
-
+    get_ascii("Tomato");
 }
 
-int get_ascii(string word)
+void get_ascii(string word)
 {
-    char
+    char c;
+    // word = tolower(word);
     for (int i = 0 ; i < strlen(word) ; i++)
     {
-
+        c = word[i];
+        printf("%d", c);
     }
 }
