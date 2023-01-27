7046752c97ad9ae87e071b6c08089b60923e454f
Theodor Nistor <115567983+Ted-Nistor@users.noreply.github.com>
2023-01-27 11:45:58 +0000

    Fri, Jan 27, 2023, 1:45 PM +02:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -5,6 +5,7 @@
 
 // Points assigned to each letter of the alphabet
 int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+int ascii_alphabet[] = {65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90};
 
 int compute_score(string word);
 
@@ -25,5 +26,11 @@ int compute_score(string word)
 {
     // TODO: Compute and return score for string
     // 1. convert each letter to upper case.
-    
+    for (int i = 0 ; i < strlen(word) ; i ++)
+    {
+        word[i] = toupper(word[i]);
+    }
+    // 2. convert each uppercase letter to ascii
+    for (int j = 0 ; )
+
 }
