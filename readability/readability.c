bc747064512bd088dcfc711aeefa105a960f98b7
Theodor Nistor <115567983+Ted-Nistor@users.noreply.github.com>
2023-01-27 19:33:56 +0000

    Fri, Jan 27, 2023, 9:33 PM +02:00

--- readability/readability.c
+++ readability/readability.c
@@ -20,9 +20,7 @@ int main(void)
     printf("%i letters\n", count_letters(paragraph));
     printf("%i words\n", count_words(paragraph));
     printf("%i sentences\n", count_sentences(paragraph));
-    index = ((letters / words) * 100.0) * 0.0588 - (((sentences / words)) * 100.0) * 0.296 - 15.8;
-    printf("Index: %f\n", index);
-
+    
 
 
 }
