c9e2ccc803499c14264867b6d10b2f6ce847dca7
Theodor Nistor <115567983+Ted-Nistor@users.noreply.github.com>
2023-01-27 12:50:58 +0000

    Fri, Jan 27, 2023, 2:50 PM +02:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -58,7 +58,6 @@ int string_to_ascii (string word)
 {
     // initialize variable that will become = to string converted into ascii
     int ascii = 0;
-    int letters = 0;
     // int new_word = 0;
     // convert each letter to upper case.
     for (int i = 0 ; i < strlen(word) ; i ++)
@@ -69,10 +68,10 @@ int string_to_ascii (string word)
     for (int j = 0 ; j < strlen(word); j ++)
     {
         ascii = word[j];
-        printf("%d",letters += ascii);
+        printf("%d",ascii);
 
     }
         printf("\n");
-        printf("Acii code is : %i\n", letters);
-        return letters;
+        printf("Acii code is : %i\n", ascii);
+        return ascii;
 }
