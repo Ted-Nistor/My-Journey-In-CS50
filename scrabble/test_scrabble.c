990835ef5a9efdb789095e06ec3dfa47697e4f72
Theodor Nistor <115567983+Ted-Nistor@users.noreply.github.com>
2023-01-27 11:39:03 +0000

    Fri, Jan 27, 2023, 1:39 PM +02:00

--- scrabble/test_scrabble.c
+++ scrabble/test_scrabble.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <ctype.h>
 
-
+int ascii_alphabet[] = {65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90};
 int main(void)
 {
     string word = get_string("Word: ");
@@ -15,9 +15,18 @@ int main(void)
     printf("the word is: %s\n", word);
     printf("the word has %i letters\n", n);
     int letter = 0;
+    int index = 0;
     for (int i = 0 ; i < n ; i++)
     {
         letter = printf("%d", word[i]);
-        return letter;
+        for (int j = 0 ; j < 25 ; j ++)
+        {
+            if (ascii_alphabet[i] == letter)
+            {
+                printf("indexes: %i, ", i);
+            }
+        }
     }
+    printf("\n");
+
 }
