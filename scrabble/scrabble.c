3594c6d4a7a209cedd8d2a939e843074d60fba49
Theodor Nistor <115567983+Ted-Nistor@users.noreply.github.com>
2023-01-27 12:09:21 +0000

    Fri, Jan 27, 2023, 2:09 PM +02:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -8,6 +8,7 @@ int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 
 int ascii_alphabet[] = {65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90};
 
 int compute_score(string word);
+int string_to_ascii (string word);
 
 int main(void)
 {
@@ -16,6 +17,8 @@ int main(void)
     string word2 = get_string("Player 2: ");
 
     // Score both words
+    int ascii1 = string_to_ascii(word1);
+    int ascii2 = string_to_ascii(word2);
     int score1 = compute_score(word1);
     int score2 = compute_score(word2);
 
@@ -29,18 +32,24 @@ int compute_score(int ascii_word)
     int index = 0;
     while (ascii_word >= 0)
     {
+        //Use a module%100 to get the last two digits from the ascii_word
         index = ascii_word % 100 ;
+        //Loop throught the ascii alphabet to match last two digits with an ascii letter from the alphabet
         for (int i = 0 ; i < 26 ; i++)
         {
             if (ascii_alphabet[i] == index)
             {
-                index = ascii_alphabet[i];
+        //If there's a match, store the index of the letter into the index variable, then use that variable to
+        // access the value from the POINTS array. Increment the score by the value of the points array.
+                index = i;
                 score+= POINTS[index];
-                break;
+                index = 0;
             }
         }
+        // Remove the last two digits from the asciiword.
+        ascii_word/100;
     }
-
+    return score;
 }
 
 int string_to_ascii (string word)
