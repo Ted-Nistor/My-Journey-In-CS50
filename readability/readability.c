6a7fcddd8ebd906f3310540a859bf5d521743814
Theodor Nistor <115567983+Ted-Nistor@users.noreply.github.com>
2023-01-27 19:06:52 +0000

    Fri, Jan 27, 2023, 9:06 PM +02:00

--- readability/readability.c
+++ readability/readability.c
@@ -5,6 +5,7 @@
 
 int count_letters(string paragraph);
 int count_words(string paragraph);
+int count_sentences(string paragraph);
 
 int main(void)
 {
@@ -14,6 +15,7 @@ int main(void)
     printf("%s\n", paragraph);
     printf("%i letters\n", count_letters(paragraph));
     printf("%i words\n", count_words(paragraph));
+    printf("%i sentences\n", count_sentences(paragraph));
 
 
 }
@@ -22,23 +24,25 @@ int main(void)
 //the character is alphabetical
 int count_letters(string paragraph)
 {
-    int counter = 0;
+    int letters = 0;
     for (int i = 0 ; i < strlen(paragraph) ; i++)
     {
         if (isalpha(paragraph[i]))
         {
-            counter++;
+            letters++;
         }
     }
-    return counter;
+    return letters;
 }
 
-//initialize a word counter
+//initialize a word counter while the index of the paragraph is less than the length of the paragraph
 int count_words(string paragraph)
 {
     int words = 0;
     int i = 0;
     do
+    // (if the character is a space) or (if the next character is a '\0' and the current character is not a space),
+    // increase the words counter by 1.
         {
             if (isspace(paragraph[i]) || (paragraph[i+1] == '\0' &&  !isspace(paragraph[i])))
             {
@@ -48,4 +52,20 @@ int count_words(string paragraph)
         }
     while (i < strlen(paragraph));
     return words;
+}
+
+//initialize a sentence counter and increment it by 1 whenever the index of the paragraph is == '.' or '?' or '!'.
+int count_sentences(string paragraph)
+{
+    int sentences = 0;
+    int i = 0;
+    do
+    {
+        if (paragraph[i] == '.' || paragraph[i] == '!' || paragraph[i] == '?')
+        {
+            sentences++;
+        }
+    }
+    while (i < strlen(paragraph));
+    return sentences;
 }
