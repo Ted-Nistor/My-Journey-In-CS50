d3bce68f213074c3bd4868a52399293705374ec3
Theodor Nistor <115567983+Ted-Nistor@users.noreply.github.com>
2023-01-27 10:41:44 +0000

    Fri, Jan 27, 2023, 12:41 PM +02:00

--- scrabble/test_scrabble.c
+++ scrabble/test_scrabble.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <cs50.h>
 #include <string.h>
+#include <ctype.h>
 
 
 int main(void)
 {
     string word = get_string("Word: ");
     int n = strlen(word);
+    word = toupper(word);
     printf("the word is: %s\n", word);
     printf("the word has %i letters\n", n);
     for (int i = 0 ; i < strlen(word); i++)
