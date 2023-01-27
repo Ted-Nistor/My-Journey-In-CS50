e49b116b4df26b9c306188f0ad5c66a17eb4be26
Theodor Nistor <115567983+Ted-Nistor@users.noreply.github.com>
2023-01-27 16:13:25 +0000

    Fri, Jan 27, 2023, 6:13 PM +02:00

--- hours/hours.c
+++ hours/hours.c
@@ -6,12 +6,14 @@ float calc_hours(int hours[], int weeks, char output);
 
 int main(void)
 {
+    int sum = 0;
     int weeks = get_int("Number of weeks taking CS50: ");
     int hours[weeks];
 
     for (int i = 0; i < weeks; i++)
     {
         hours[i] = get_int("Week %i HW Hours: ", i);
+        sum += hours[i];
     }
 
     char output;
@@ -19,7 +21,18 @@ int main(void)
     {
         output = toupper(get_char("Enter T for total hours, A for average hours per week: "));
     }
-   
+    while (output != 'T' || output != 'A');
+
+    if (output == 'T')
+    {
+        printf("%c = %i\n", output, sum);
+    }
+    else
+    {
+        printf("%c = %i\n", output ,sum / weeks);
+    }
+
+
 
     // printf("%.1f hours\n", calc_hours(hours, weeks, output));
 }
