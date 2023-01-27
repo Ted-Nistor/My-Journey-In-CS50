d8d4ee9718bf3e1e0cba4818db3420cdb9bc73f7
Theodor Nistor <115567983+Ted-Nistor@users.noreply.github.com>
2023-01-27 10:19:10 +0000

    Fri, Jan 27, 2023, 12:19 PM +02:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -5,7 +5,7 @@
 
 // Points assigned to each letter of the alphabet
 int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
-
+int alphabet = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
 int compute_score(string word);
 
 int main(void)
@@ -24,4 +24,5 @@ int main(void)
 int compute_score(string word)
 {
     // TODO: Compute and return score for string
+    word = tolower(word);
 }
