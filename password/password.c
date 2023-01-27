43af54ea24984d76131f03d23b3a12792b49fbdd
Theodor Nistor <115567983+Ted-Nistor@users.noreply.github.com>
2023-01-27 16:48:49 +0000

    Fri, Jan 27, 2023, 6:48 PM +02:00

--- password/password.c
+++ password/password.c
@@ -4,6 +4,8 @@
 
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 
 bool valid(string password);
 
@@ -23,5 +25,35 @@ int main(void)
 // TODO: Complete the Boolean function below
 bool valid(string password)
 {
+    int upper = 0, lower = 0, number = 0, symbol = 0;
+    for (int i = 0 ; i < strlen(password) ; i++)
+    {
+        if (isalpha(password[i]))
+        {
+            if(password[i] == tolower(password[i]))
+            {
+                lower++;
+            }
+            else if (password[i] == toupper(password[i]))
+            {
+                upper++;
+            }
+        }
+        else if (isdigit(password[i]))
+        {
+            number++;
+        }
+        else
+        {
+            symbol++;
+        }
+    }
+    if (upper >= 1 && lower >=1 && number >= 1 && symbol >= 1)
+    {
+        return true;
+    }
+    else
+    {
     return false;
+    }
 }
