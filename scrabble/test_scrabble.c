babdbb1d3b1096180d9a64d61e6c3fb6b1505798
Theodor Nistor <115567983+Ted-Nistor@users.noreply.github.com>
2023-01-27 10:29:20 +0000

    Fri, Jan 27, 2023, 12:29 PM +02:00

---
+++ scrabble/test_scrabble.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+
+
+int main(void)
+{
+
+}
+
+int get_ascii(string word)
+{
+    char
+    for (int i = 0 ; i < strlen(word) ; i++)
+    {
+
+    }
+}
