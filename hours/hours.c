0d27a08af8887cf815a3430d7b0f51dc07245236
Theodor Nistor <115567983+Ted-Nistor@users.noreply.github.com>
2023-01-27 15:52:10 +0000

    Fri, Jan 27, 2023, 5:52 PM +02:00

--- hours/hours.c
+++ hours/hours.c
@@ -2,7 +2,7 @@
 #include <ctype.h>
 #include <stdio.h>
 
-float calc_hours(int hours[], int weeks, char output);
+// float calc_hours(int hours[], int weeks, char output);
 
 int main(void)
 {
@@ -21,11 +21,11 @@ int main(void)
     }
     while (output != 'T' && output != 'A');
 
-    printf("%.1f hours\n", calc_hours(hours, weeks, output));
+    // printf("%.1f hours\n", calc_hours(hours, weeks, output));
 }
 
-// TODO: complete the calc_hours function
-float calc_hours(int hours[], int weeks, char output)
-{
-   
-}
+// // TODO: complete the calc_hours function
+// float calc_hours(int hours[], int weeks, char output)
+// {
+
+// }
