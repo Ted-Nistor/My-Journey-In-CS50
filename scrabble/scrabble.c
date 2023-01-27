7c57e8bb09ea599ce20636e1ea1f46506ba42f8e
Theodor Nistor <115567983+Ted-Nistor@users.noreply.github.com>
2023-01-27 12:12:55 +0000

    Fri, Jan 27, 2023, 2:12 PM +02:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -7,24 +7,24 @@
 int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 int ascii_alphabet[] = {65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90};
 
-int compute_score(string word);
+int compute_score(int ascii_word);
 int string_to_ascii (string word);
 
 int main(void)
 {
     // Get input words from both players
     string word1 = get_string("Player 1: ");
-    string word2 = get_string("Player 2: ");
+    // string word2 = get_string("Player 2: ");
 
     // Score both words
     int ascii1 = string_to_ascii(word1);
-    int ascii2 = string_to_ascii(word2);
+    // int ascii2 = string_to_ascii(word2);
     int score1 = compute_score(ascii1);
-    int score2 = compute_score(ascii2);
+    // int score2 = compute_score(ascii2);
 
     // TODO: Print the winner
     printf("Score 1 : %d\n", score1);
-    printf("Score 2 : %d\n", score2);
+    // printf("Score 2 : %d\n", score2);
 }
 
 int compute_score(int ascii_word)
@@ -49,7 +49,7 @@ int compute_score(int ascii_word)
             }
         }
         // Remove the last two digits from the asciiword.
-        ascii_word/100;
+        ascii_word /= 100;
     }
     return score;
 }
