c5ee6d6ca080ab7ce41d99401bb53edfe683f067
Theodor Nistor <115567983+Ted-Nistor@users.noreply.github.com>
2023-01-29 19:04:59 +0000

    Sun, Jan 29, 2023, 9:04 PM +02:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -6,5 +6,21 @@
 
 int main(int argc, int arvg[])
 {
-    
+
+}
+
+bool validate_key(string s)
+{
+    if (strlen(s) < 26)
+    {
+        printf("Key must contain 26 characters.\n");
+        return 1;
+    }
+    for (int i = 0 ; i < strlen(s) ; i++)
+    {
+        if(!isalpha(s))
+        {
+            prinf("Usage: ./substitution key\n");
+        }
+    }
 }
