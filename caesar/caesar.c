f8656411e01ee58fc1665802c409afffbf0bd84d
Theodor Nistor <115567983+Ted-Nistor@users.noreply.github.com>
2023-01-29 14:40:16 +0000

    Sun, Jan 29, 2023, 4:40 PM +02:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -1,11 +1,16 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, string argv[])
 {
     if (argc == 2)
     {
         return 0;
+    }
+    else if (atoi(argv[1]))
+    {
+        
     }
     else
     {
