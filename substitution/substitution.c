a8e26588cfa3ff32a59e36a9dfef87ceaa086b7b
Theodor Nistor <115567983+Ted-Nistor@users.noreply.github.com>
2023-01-29 23:52:27 +0000

    Mon, Jan 30, 2023, 1:52 AM +02:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -65,20 +65,21 @@ string cipher_alphabet(string key, string plaintext)
 
     for (int j = 0 ; j < strlen(plaintext) ; j++)
     {
-            if (isalpha(plaintext[i]))
+            if (isalpha(plaintext[j]))
             {
-                if (isupper(plaintext[i]))
+                if (isupper(plaintext[j]))
                 {
-                index = plaintext[i] % 65;
+                index = plaintext[j] % 65;
                 }
-                else if (islower(plaintext[i]))
+                else if (islower(plaintext[j]))
                 {
-                index = plaintext[i] % 97;
+                index = plaintext[j] % 97;
                 }
                 plaintext[j] = cipher[index];
             }
 
     }
+    return plaintext;
 }
 
 
