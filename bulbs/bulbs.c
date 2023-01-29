e99b0b6d6edc074742da78d86964b66254e27ad4
Theodor Nistor <115567983+Ted-Nistor@users.noreply.github.com>
2023-01-29 12:43:57 +0000

    Sun, Jan 29, 2023, 2:43 PM +02:00

--- bulbs/bulbs.c
+++ bulbs/bulbs.c
@@ -32,31 +32,53 @@ void print_bulb(int bit)
 
 void to_binary(string word)
 {
+    //initialize an ascii variable that equals to the ascii format
+    // of a letter
+    // initialize a binary variable that can be equal to either
+    // 1 or 2
     int ascii = 0;
     int binary = 0;
+    //Convert each letter to its ascii format by looping through
+    //each letter
     for (int i = 0 ; i < strlen(word) ; i++)
     {
         ascii = word[i];
+        // make a loop that runs from 2^7 to 2^0 (the bits allocated)
+        // for each letter
         for (int j = 7 ; j >= 0 ; j --)
         {
+            //Make a greedy algorithm where you compare if the
+            //ascii letter with the bits allocated.
             int bits = pow(2, j);
+            // if the ascii letter is equal to the number of bits
+            // the binary variable becomes 1.
+            // Don't forget to get the remainder of the ascii % bits
+            // This becomes the new ascii variable
             if (ascii == bits)
             {
                 binary = 1;
                 ascii %= bits;
             }
+            // If ascii variable is lower than bits , assign the
+            // binary variable to 0
             else if (ascii < bits)
             {
                 binary = 0 ;
             }
             else
             {
-
+                // if the ascii variable is equal to the number
+                // of bits, assign the binary variable to 1 and
+                // get the remainder of ascii % bits.
+                //
                 binary = 1 ;
                 ascii %= bits;
             }
+            // Once you get the binary variable equal to either
+            // 1 or 0, use the print_bulb function and pass
+            // the binary variable as its argument.
             print_bulb(binary);
         }
-            printf("\n");
+        printf("\n");
     }
 }
