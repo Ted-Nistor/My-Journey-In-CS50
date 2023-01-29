20cee8a0cf5a138f3b2706b2ddeda9726f6f180d
Theodor Nistor <115567983+Ted-Nistor@users.noreply.github.com>
2023-01-29 14:05:06 +0000

    Sun, Jan 29, 2023, 4:05 PM +02:00

--- no-vowels/no-vowels.c
+++ no-vowels/no-vowels.c
@@ -5,11 +5,14 @@
 
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
+void replace(string argv[1]);
 int main(int argc, string argv[])
 {
     if (argc == 2)
     {
+        replace(argv[1]);
         return 0;
     }
     else
@@ -17,4 +20,28 @@ int main(int argc, string argv[])
         printf("Missing or exceeding command line\n");
         return 1;
     }
+}
+
+void replace(string argv[1])
+{
+    for (int i = 0 ; i < strlen(argv[1]) ; i++)
+    {
+        if (argv[1][i] == 'A' || argv[1][i] == 'a' )
+        {
+            argv[1][i] = '6';
+        }
+        else if (argv[1][i] == 'E' || argv[1][i] == 'e')
+        {
+            argv[1][i] = '3';
+        }
+        else if (argv[1][i] == 'I' || argv[1][i] == 'i')
+        {
+            argv[1][i] = '1';
+        }
+        else if (argv[1][i] == 'O' || argv[1][i] == 'o')
+        {
+            argv[1][i] = '0';
+        }
+    }
+    printf("%s\n", argv[1]);
 }
