ac64015abddc1bd9542a9db71f15c9e027a277f8
Theodor Nistor <115567983+Ted-Nistor@users.noreply.github.com>
2023-01-29 18:40:26 +0000

    Sun, Jan 29, 2023, 8:40 PM +02:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -1,7 +1,10 @@
-#include <cs50.h>
 #include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
 
-int main(int argc, string argv[]) 
+int main(int argc, int arvg[])
 {
-
+    
 }
