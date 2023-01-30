b92c627effdb937b09ce3f52af5e5e39d1014b74
Theodor Nistor <115567983+Ted-Nistor@users.noreply.github.com>
2023-01-30 19:10:26 +0000

    Mon, Jan 30, 2023, 9:10 PM +02:00

--- phonebook.c
+++ phonebook.c
@@ -5,4 +5,17 @@
 int main (void)
 {
     string names[] = {"Carter", "David"};
+    string numbers[]= {"+1-617-495-1000", "+1-949-468-2750"};
+
+    string name = get_string("Name: ");
+    for (int i = 0 ; i < 2 ; i++)
+    {
+        if (strcmp(names[i], name) == 0)
+        {
+            printf("Found %s\n", numbers[1]);
+            return 0;
+        }
+    }
+    printf("Not found\n");
+    return 1;
 }
