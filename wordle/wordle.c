97c8e8b25d9f5030dfac78a4c66f841d97d8e641
Theodor Nistor <115567983+Ted-Nistor@users.noreply.github.com>
2023-01-30 12:07:46 +0000

    Mon, Jan 30, 2023, 2:07 PM +02:00

--- wordle/wordle.c
+++ wordle/wordle.c
@@ -26,7 +26,7 @@ void print_word(string guess, int wordsize, int status[]);
 
 int main(int argc, string argv[])
 {
-    printf(GREEN"This is WORDLE50"RESET"\n");
+    
     // ensure proper usage
     // TODO #1
     if (argc != 2)
