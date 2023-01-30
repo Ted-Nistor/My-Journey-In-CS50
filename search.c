f2316905c4406ed817a6fe3b36932817ec8f805f
Theodor Nistor <115567983+Ted-Nistor@users.noreply.github.com>
2023-01-30 18:34:47 +0000

    Mon, Jan 30, 2023, 8:34 PM +02:00

--- search.c
+++ search.c
@@ -5,4 +5,13 @@ int main(void)
 {
     // static array
     int numbers [] = {20, 500, 10, 5, 100, 1, 50};
+    // or
+    // int numbers[7];
+    // numbers[0] = 20;
+    // numbers[1] = 500;
+    // ... and so on --- same thing
+
+    int n = get_int("Number: ");
+    //linear search
+    for (int i = 0 ;)
 }
