a14a56ca931a5ac7b0e83fe9750024578b7c4e56
Theodor Nistor <115567983+Ted-Nistor@users.noreply.github.com>
2023-01-30 18:41:28 +0000

    Mon, Jan 30, 2023, 8:41 PM +02:00

--- search.c
+++ search.c
@@ -4,23 +4,36 @@
 int main(void)
 {
     // static array
-    int numbers [] = {20, 500, 10, 5, 100, 1, 50};
+    // int numbers [] = {20, 500, 10, 5, 100, 1, 50};
+    string strings[] = {"battleship", "boot", "cannon", "iron", "thimble", "top hat"};
     // or
     // int numbers[7];
     // numbers[0] = 20;
     // numbers[1] = 500;
     // ... and so on --- same thing
 
-    int n = get_int("Number: ");
+    // int n = get_int("Number: ");
+    string s = get_string("String: ")l
     //linear search
-    for (int i = 0 ; i < 7 ; i++)
+    // for (int i = 0 ; i < 7 ; i++)
+    // {
+    //     if (numbers[i] == n)
+    //     {
+    //         printf("Found\n");
+    //         return 0;
+    //     }
+    // }
+    // printf("Not found\n");
+    // return 1;
+    // for the strings example
+    for (int i = 0 ; i < 7 ; i ++)
     {
-        if (numbers[i] == n)
+        if (strings[i] == s)
         {
             printf("Found\n");
             return 0;
         }
+        printf("Not found\n");
+        return 1;
     }
-    printf("Not found\n");
-    return 1;
 }
