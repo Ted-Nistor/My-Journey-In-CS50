8608d9e9545a2f1116008a11d83f027ae2006c85
Theodor Nistor <115567983+Ted-Nistor@users.noreply.github.com>
2023-01-30 08:50:15 +0000

    Mon, Jan 30, 2023, 10:50 AM +02:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -45,12 +45,12 @@ bool validate_string(string s)
         }
         for (int j = i + 1 ; j < strlen(s) ; j++)
         {
-           if (s[i] == s[j])
-           {
-            printf("Usage: ./substitution key must not contain repeating characters\n");
-            check = 0;
-            break;
-           }
+            if (s[i] == s[j])
+            {
+                printf("Usage: ./substitution key must not contain repeating characters\n");
+                check = 0;
+                break;
+            }
         }
     }
     return check == 1 ? true : false;
