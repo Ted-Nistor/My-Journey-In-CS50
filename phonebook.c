cb1bd8a5ab17469e2d977f85695dbb9358c9372b
Theodor Nistor <115567983+Ted-Nistor@users.noreply.github.com>
2023-01-30 19:20:39 +0000

    Mon, Jan 30, 2023, 9:20 PM +02:00

--- phonebook.c
+++ phonebook.c
@@ -2,10 +2,24 @@
 #include <cs50.h>
 #include <string.h>
 
+typedef struct
+{
+    string name;
+    string number;
+}
+person;
+
 int main (void)
 {
-    string names[] = {"Carter", "David"};
-    string numbers[]= {"+1-617-495-1000", "+1-949-468-2750"};
+    person people[2];
+    people[0].name = "Carter";
+    people[0].number = "+1-617-495-100";
+    people[1].name = "David";
+    people[1].number = 
+    // first example
+    // string names[] = {"Carter", "David"};
+    // string numbers[]= {"+1-617-495-1000", "+1-949-468-2750"};
+
 
     string name = get_string("Name: ");
     for (int i = 0 ; i < 2 ; i++)
