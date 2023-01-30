e9cbe23fd429c6dc8a32c403426d4d1f583d2f63
Theodor Nistor <115567983+Ted-Nistor@users.noreply.github.com>
2023-01-30 08:54:46 +0000

    Mon, Jan 30, 2023, 10:54 AM +02:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -24,25 +24,28 @@ int main(int argc, string argv[])
     return 0;
 }
 
-
+// Function to validate the string
 bool validate_string(string s)
 {
+    //If everything is ok, the check should remain equal to 1
     int check = 1 ;
-    int repeated = 0;
-    int index = 0;
+    // First check if the string has 26 characters
     if (strlen(s) != 26)
     {
         check = 0;
         printf("Usage: ./substitution key must containt 26 characters\n");
     }
+    // Then loop through each character
     for (int i = 0; i < strlen(s) ; i++)
     {
+        // check if the character is alphabetical
         if (!isalpha(s[i]))
         {
             printf("Usage: ./substitution key must contain only alphabetical characters\n");
             check = 0;
             break;
         }
+        // Create a second loop starting from i+1 and check if the characters are repeating
         for (int j = i + 1 ; j < strlen(s) ; j++)
         {
             if (s[i] == s[j])
@@ -56,9 +59,10 @@ bool validate_string(string s)
     return check == 1 ? true : false;
 }
 
-
+//Function to conver the string to cipher text
 string cipher_text(string key, string plaintext)
 {
+    // Initialize a 26 elements array for the cipher
     int index = 0 ;
     int cipher[26];
     for (int i = 0 ; i < strlen(key) ; i++)
