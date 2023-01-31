161354a2267c4f56b7a2f084510d16e58c41187a
Theodor Nistor <115567983+Ted-Nistor@users.noreply.github.com>
2023-01-31 19:43:40 +0000

    Tue, Jan 31, 2023, 9:43 PM +02:00

--- temps/temps.c
+++ temps/temps.c
@@ -65,14 +65,22 @@ void sort_cities(void)
     // Add your code here
     int counter = -1;
     int i = 0;
+    int j;
+    string s;
     while (counter != 0)
     {
         counter = 0;
-        if (temps[i].temp < temps[i + 1].temp)
+        if (temps[i].temp <= temps[i + 1].temp)
         {
             counter++;
-            
+            j = temps[i].temp;
+            temps[i].temp = temps[i + 1].temp;
+            temps[i + 1].temp = j;
+            s = temps[i].city;
+            temps[i].city = temps[i + 1].city;
+            temps[i + 1].city = s;
         }
+        i++;
     }
 
 }
