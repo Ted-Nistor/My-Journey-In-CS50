e9e4df2e8c2026ac27a7fd6c9773fc69d042862b
Theodor Nistor <115567983+Ted-Nistor@users.noreply.github.com>
2023-02-01 14:34:05 +0000

    Wed, Feb 1, 2023, 4:34 PM +02:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -84,7 +84,7 @@ void print_winner(void)
     for (int i = 0; i < candidate_count; i++)
     {
         {
-            for (int j = 0; j < candidate_count; j++)
+            for (int j = 1; j < candidate_count; j++)
             {
                 if (candidates[i].votes > candidates[j].votes)
                 {
