a53bab4b2687f7f1cf7bf52695aa80e9ac6d5023
Theodor Nistor <115567983+Ted-Nistor@users.noreply.github.com>
2023-02-01 12:55:34 +0000

    Wed, Feb 1, 2023, 2:55 PM +02:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -81,13 +81,12 @@ bool vote(string name)
 // Print the winner (or winners) of the election
 void print_winner(void)
 {
-    int highest_votes = 0;
     for (int i = 0 ; i < candidate_count - 1 ; i++)
     {
-        highest_votes = candidates[i].votes
-        if (highest_votes > candidates[i + 1])
+        int highest_votes = candidates[i].votes;
+        if (highest_votes < candidates[i + 1].votes)
         {
-            highest_votes = candidates[i + 1];
+            highest_votes = candidates[i + 1].votes;
         }
 
         printf("Candidate: %s Votes: %i\n", candidates[i].name , highest_votes);
