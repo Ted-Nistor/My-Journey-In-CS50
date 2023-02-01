d16d791a5b80c16fb911ce00ab3e6f523a0d4176
Theodor Nistor <115567983+Ted-Nistor@users.noreply.github.com>
2023-02-01 13:30:07 +0000

    Wed, Feb 1, 2023, 3:30 PM +02:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -90,19 +90,18 @@ void print_winner(void)
         counter = 0;
         for (int i = 0; i < n ; i ++)
         {
-            if (candidates[i].votes > candidates[i + 1].votes)
-            {
-                counter++;
-                temp = candidates[i].votes;
-                candidates[i].votes = candidates[i + 1].votes;
-                candidates[i + 1].votes = temp;
-                s = candidates[i].name;
-                candidates[i].name = candidates[i + 1].name;
-                candidates[i + 1].name = s;
+           if (candidates[i].votes > candidates[i + 1].votes)
+           {
+               counter++;
+               temp = candidates[i].votes;
+               candidates[i].votes = candidates[i + 1].votes;
+               candidates[i + 1].votes = temp;
+               s = candidates[i].name;
+               candidates[i].name = candidates[i + 1].name;
+               candidates[i + 1].name = s;
             }
-        printf ("Candidate %s Votes: %i\n", candidates[i].name, candidates[i].votes);
         }
-
     }
+
     return;
 }
