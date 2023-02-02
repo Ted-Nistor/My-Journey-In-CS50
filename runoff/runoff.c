39b101e710bcec9d0b5d5c613022ae32da6b178a
Theodor Nistor <115567983+Ted-Nistor@users.noreply.github.com>
2023-02-02 11:00:46 +0000

    Thu, Feb 2, 2023, 1:00 PM +02:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -131,7 +131,7 @@ bool vote(int voter, int rank, string name)
 {
     for(int i=0; i < candidate_count; i++) // check if candidate is valid
      {
-        if(strcmp(name,candidates[i].name) == 0)
+        if(strcmp(name, candidates[i].name) == 0)
         {
             preferences[voter][rank]=i;
             return true;
