8669af9c7c7ff3a8ee9d7f645d48c746755f8c93
Theodor Nistor <115567983+Ted-Nistor@users.noreply.github.com>
2023-02-02 13:38:50 +0000

    Thu, Feb 2, 2023, 3:38 PM +02:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -115,7 +115,7 @@ bool vote(int rank, string name, int ranks[])
 void record_preferences(int ranks[])
 {
 
-    for (int i = 0 ; i < voters_count; i++)
+    for (int i = 0 ; i < voter_count; i++)
     {
         for (int j = 0; j < candidate_count; j++)
         {
