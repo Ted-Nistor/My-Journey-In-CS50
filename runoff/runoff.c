56f9ea358272bc6187f255354322625867328fe4
Theodor Nistor <115567983+Ted-Nistor@users.noreply.github.com>
2023-02-02 09:58:23 +0000

    Thu, Feb 2, 2023, 11:58 AM +02:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -134,7 +134,6 @@ bool vote(int voter, int rank, string name)
         for (rank = 0; rank < candidate_count; rank++)
         {
             if (strcmp(candidates[rank].name, name) == 0)
-            candidates[rank].votes++;
             return true;
         }
     }
