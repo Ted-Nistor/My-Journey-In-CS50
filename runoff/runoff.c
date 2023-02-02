2ca06e5e77ec377ec962e2070229f17b35f17cb2
Theodor Nistor <115567983+Ted-Nistor@users.noreply.github.com>
2023-02-02 11:11:26 +0000

    Thu, Feb 2, 2023, 1:11 PM +02:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -129,84 +129,100 @@ int main(int argc, string argv[])
 // Record preference if vote is valid
 bool vote(int voter, int rank, string name)
 {
-    for(int i=0; i < candidate_count; i++) // check if candidate is valid
+    // TODO
+     for(int i=0;i<candidate_count;i++) // check if candidate is valid
      {
-        if(strcmp(name, candidates[i].name) == 0)
-        {
-            preferences[voter][rank]=i;
-            return true;
-        }
+   if
+      ( strcmp(name,candidates[i].name) == 0)
+   { preferences[voter][rank]=i;
+
+        return true;
+    }
      }
-    return false;
+   return false;
 }
 
 // Tabulate votes for non-eliminated candidates
 void tabulate(void)
 {
-    for (int i = 0; i < voter_count; i++)
+    // TODO
+     for(int i=0;i<voter_count;i++) // for looping over voters
     {
-        for(int j = 0; j < candidate_count; j++)
-        {
-            if(candidates[i].eliminated == false)
-            {
-                candidates[preferences[i][j]].votes++;
-            }
-        }
+        for(int j=0;j<candidate_count;j++) // to loop over candidates
+  {
+      if(candidates[i].eliminated==false) // if candidate is not eliminated
+   {
+        candidates[preferences[i][j]].votes++;
     }
+  }
+    }
+
     return;
 }
 
 // Print the winner of the election, if there is one
 bool print_winner(void)
 {
-    for(int i=0;i < candidate_count;i++) // loop thru candidates
-    {
-        string most= candidates[i].name ;// for most votes
-        if(candidates[i].votes > voter_count/2) // more than 50 %
-        {
-            printf("%s\n",most);
-        }
-    }
+    // TODO
+
+    // for highest votes
+    for(int i=0;i<candidate_count;i++) // loop thru candidates
+{
+    string most= candidates[i].name ;// for most votes
+    if(candidates[i].votes>voter_count/2) // more than 50 %
+{
+    printf("winner %s\n",most);
+}
+}
 
     return false;
+
 }
 
 // Return the minimum number of votes any remaining candidate has
 int find_min(void)
 {
-    int min = voter_count;
-    for (int i = 0; i < candidate_count; i++)
+    // TODO
+    int minvotes=voter_count; // minimum votes is equal to number of voters
+
+    for(int i=0;i<candidate_count;i++) // loop thru candidates
     {
-        if (candidates[i].eliminated == false && candidates[i].votes > min)
-        {
-            min = candidates[i].votes;
-        }
+        if(candidates[i].eliminated==false && candidates[i].votes> minvotes)
+    {
+        minvotes=candidates[i].votes;
     }
+    }
+
     return 0;
 }
 
+
+
 // Return true if the election is tied between all candidates, false otherwise
-bool is_tie(int min)
+bool is_tie(int minvotes)
 {
-    for (int i = 0 ; i < candidate_count; i++)
-    {
-        if (candidates[i].eliminated == false && candidates[i].votes != min)
-        {
-            return false;
-        }
-    }
-    return true;
+    // TODO
+
+     for(int i=0;i<candidate_count;i++) // loop thru candidates
+     {
+         if(candidates[i].eliminated==false && candidates[i].votes!=minvotes) // candidates are not eliminated and their votes are not equal to minvotes then theres no tie, lowest gets eliminated
+         {
+      return false;
+}
+}
+return true; // but if votes are equal to minvotes then there is a tie
 }
 
-// Eliminate the candidate (or candidates) in last place
-void eliminate(int min)
+// Eliminate the candidate (or candidiates) in last place
+void eliminate(int minvotes)
 {
-    for (int i = 0; i < candidate_count; i++)
-    {
-        if (candidates[i].eliminated == false && candidates[i].votes == min )
+    // TODO
+    for(int i=0;i<candidate_count;i++) // loop thru candidates
+        if (candidates[i].eliminated==false && candidates[i].votes==minvotes)
         {
-            candidates[i].eliminated = true;
+            candidates[i].eliminated=true;
         }
-    }
+
     return;
+
 }
