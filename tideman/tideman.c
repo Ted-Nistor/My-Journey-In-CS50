3f6d258fe68a9f8a73dc4c3acb6ca185cbe2a790
Theodor Nistor <115567983+Ted-Nistor@users.noreply.github.com>
2023-02-02 13:42:43 +0000

    Thu, Feb 2, 2023, 3:42 PM +02:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -120,7 +120,6 @@ void record_preferences(int ranks[])
         for (int j = i + 1; j < candidate_count; j++)
         {
             preferences[ranks[i]][ranks[j]]++;
-            break;
         }
     }
     return;
