6f0857a42263a1e1555eaade7fd292814cbad2d6
Theodor Nistor <115567983+Ted-Nistor@users.noreply.github.com>
2023-02-02 10:02:41 +0000

    Thu, Feb 2, 2023, 12:02 PM +02:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -144,7 +144,10 @@ bool vote(int voter, int rank, string name)
 // Tabulate votes for non-eliminated candidates
 void tabulate(void)
 {
-    // TODO
+    for (int voter = 0; voter < voter_count; voter++)
+    {
+        for (int rank = 0; rank < candidate_count; rank++)
+    }
     return;
 }
 
