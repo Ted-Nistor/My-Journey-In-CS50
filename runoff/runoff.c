11723267ecd6efda791bc0549e9f3017b2a23ee3
Theodor Nistor <115567983+Ted-Nistor@users.noreply.github.com>
2023-02-02 10:45:51 +0000

    Thu, Feb 2, 2023, 12:45 PM +02:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -183,14 +183,24 @@ bool print_winner(void)
 // Return the minimum number of votes any remaining candidate has
 int find_min(void)
 {
-    // TODO
+    int min_votes = voter_count;
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (candidates[i].eliminated == false && candidates[i].votes > min_votes)
+        {
+            min_votes = candidates[i].votes;
+        }
+    }
     return 0;
 }
 
 // Return true if the election is tied between all candidates, false otherwise
 bool is_tie(int min)
 {
-    // TODO
+    for (int i = 0 ; i < candidate_count; i++)
+    {
+        if (candidate[i].eliminated == false && )
+    }
     return false;
 }
 
